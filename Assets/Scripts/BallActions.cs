99272be72df381ba3398baa5b244e8267c203702
hzh <hzh569@163.com>
2017-10-14 15:02:40 +0800

    First Version

---
+++ Assets/Scripts/BallActions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallActions : MonoBehaviour {
+
+    public GameObject board;
+    public float offset;
+    public float ShootSpeed;
+    public float move;
+
+    private bool started;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        started = false;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!started)
+        {
+            transform.position = board.transform.position + new Vector3(0, offset, 0);
+            if (Input.GetKey("space"))
+            {
+                rb.velocity = new Vector3(0, ShootSpeed, 0);
+                started = true;
+            }
+        }
+	}
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Board")
+        {
+            Vector3 offset = transform.position - board.transform.position;
+            rb.velocity += new Vector2(offset.x * move, 0);
+        }
+        else if (collision.gameObject.name == "Bottom")
+        {
+            started = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MoveBoard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBoard : MonoBehaviour {
+
+    public float MoveSpeed;
+    public float border;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float ix = Input.acceleration.x;
+        float h = Input.GetAxis("Horizontal");
+        float x = transform.position.x;
+
+        if (h != 0)
+            transform.Translate(Vector3.right * h * MoveSpeed);
+        if (ix != 0)
+            transform.Translate(Vector3.right * ix * MoveSpeed);
+        if (transform.position.x > border)
+            transform.position = new Vector3(border, transform.position.y, transform.position.z);
+        if (transform.position.x < -border)
+            transform.position = new Vector3(-border, transform.position.y, transform.position.z);
+    }
+}
